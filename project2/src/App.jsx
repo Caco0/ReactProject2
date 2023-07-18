479c9f391a53b9721bc69c0bb56efdadf1c6151e
Rafael Dametto <cacotpds@gmail.com>
2023-07-18 16:07:13 -0300

    commit useMemo

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,32 +1,52 @@
 import P from 'prop-types';
-import { useCallback, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-const Button = ({ incrementButton }) => {
-  console.log('Filho Renderizou!');
-  return <button onClick={() => incrementButton(100)}> + </button>;
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
-Button.propTypes = { incrementButton: P.func };
-
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  console.log('Pai renderizou!');
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  //Component did mount
+  useEffect(() => {
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  const btn = useMemo(() => {
-    return <Button incrementButton={incrementCounter} />;
-  }, [incrementCounter]);
-
-  console.log('Pai renderizou');
   return (
     <>
       <div>
-        <p>Teste 1</p>
-        <h1>C1: {counter}</h1>
-        {btn}
+        <p>
+          <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+        </p>
+        {useMemo(() => {
+          return (
+            posts.length > 0 &&
+            posts.map((post) => {
+              return <Post key={post.id} post={post} />;
+            })
+          );
+        }, [posts])}
+        {posts.length <= 0 && <span className="load">Carregando...</span>}
       </div>
     </>
   );
