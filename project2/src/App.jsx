570512a4540c899647c9c19e6eee5de18debcfcd
Rafael Dametto <cacotpds@gmail.com>
2023-07-19 10:34:00 -0300

    useContext

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,79 +1,128 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
 
-const Post = ({ post, handleClick }) => {
+const globalState = {
+  title: 'O titulo do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+const GlobalContext = React.createContext();
+
+//eslint-disable-next-line
+const Div = ({ children }) => {
   return (
-    <div key={post.id} className="post">
-      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
-        {post.title}
-      </h1>
-      <p>{post.body}</p>
-    </div>
+    <>
+      <H1 />
+      <P />
+    </>
   );
 };
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
+
+//eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  return (
+    <h1>
+      {title} {counter}
+    </h1>
+  );
+};
+
+//eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body, counter },
+    contextState,
+    setContextState,
+  } = theContext;
+  return <p onClick={() => setContextState({ ...contextState, counter: counter + 1 })}>{body}</p>;
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-  const contador = useRef(0);
-
-  console.log('Pai renderizou!');
-
-  //Component did mount
-  useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  useEffect(() => {
-    contador.current++;
-  });
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
-
+  const [contextState, setContextState] = useState(globalState);
   return (
-    <>
-      <div>
-        <h3>Renderizou {contador.current}X</h3>
-        <p>
-          <input
-            ref={input}
-            type="search"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </p>
-        {useMemo(() => {
-          return (
-            posts.length > 0 &&
-            posts.map((post) => {
-              return <Post key={post.id} post={post} handleClick={handleClick} />;
-            })
-          );
-        }, [posts])}
-        {posts.length <= 0 && <span className="load">Carregando...</span>}
-      </div>
-    </>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div></Div>
+    </GlobalContext.Provider>
   );
 }
+
+// const Post = ({ post, handleClick }) => {
+//   return (
+//     <div key={post.id} className="post">
+//       <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+//         {post.title}
+//       </h1>
+//       <p>{post.body}</p>
+//     </div>
+//   );
+// };
+// Post.propTypes = {
+//   post: P.shape({
+//     id: P.number,
+//     title: P.string,
+//     body: P.string,
+//   }),
+//   handleClick: P.func,
+// };
+
+// function App() {
+//   const [posts, setPosts] = useState([]);
+//   const [value, setValue] = useState('');
+//   const input = useRef(null);
+//   const contador = useRef(0);
+
+//   console.log('Pai renderizou!');
+
+//   //Component did mount
+//   useEffect(() => {
+//     setTimeout(function () {
+//       fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then((r) => r.json())
+//         .then((r) => setPosts(r));
+//     }, 1000);
+//   }, []);
+
+//   useEffect(() => {
+//     input.current.focus();
+//     console.log(input.current);
+//   }, [value]);
+
+//   useEffect(() => {
+//     contador.current++;
+//   });
+
+//   const handleClick = (value) => {
+//     setValue(value);
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <h3>Renderizou {contador.current}X</h3>
+//         <p>
+//           <input
+//             ref={input}
+//             type="search"
+//             value={value}
+//             onChange={(e) => setValue(e.target.value)}
+//           />
+//         </p>
+//         {useMemo(() => {
+//           return (
+//             posts.length > 0 &&
+//             posts.map((post) => {
+//               return <Post key={post.id} post={post} handleClick={handleClick} />;
+//             })
+//           );
+//         }, [posts])}
+//         {posts.length <= 0 && <span className="load">Carregando...</span>}
+//       </div>
+//     </>
+//   );
+// }
 export default App;
